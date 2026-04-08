6d9f46ac4e77cc1a96ae39e321643b35b8753731
hasimliali7 <hasimliali07@gmail.com>
2026-04-08 11:41:28 +0000

    Task 2: Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - floor module.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the float n as an integer.
+    """
+    return int(n)
